1fde21667905bebce15b464f29b9d195d1cd60e4
Felix Finkeldey <felix.finkeldey@tu-dortmund.de>
2020-02-04 13:27:20 +0100

    Finished processing routine

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,6 @@
 """
-Process raw data by adding pose features to target filenames
+Process raw data by combining pose features with frequency features
+for the prediction of each amplitude of each measurement direction.
 """
 
 import os
@@ -13,6 +14,7 @@ import misc
 
 def main():
     """Main method"""
+    # Definitions of data and plotting properties
     data_dir = '../data/01_raw'
     processed_dir = '../data/02_processed'
     plot_dir = '../figures'
@@ -20,38 +22,73 @@ def main():
     doe_file = '../data/01_raw/Versuchsplan.xlsx'
     figsize = (7, 7)
     fontsize = 14
-    x_range = (200, 3201, 1000)
+    freq_step = 0.25 # Precision of the measurement
+    x_range = (200, 3200, 1000)
     y_range = (0, 0.9, 0.2)
+    input_size = 4
+    output_size = 2
 
+    # Fetch all filenames for YY FRFs
     filenames = [
         filename for filename in os.listdir(data_dir)
         if filename.endswith('.txt') and filename.split('_')[2] == 'YY'
     ]
 
+    # Read poses from design of experiments file
     positions = pd.read_excel(doe_file, sheet_name='positions')
     doe = pd.read_excel(doe_file, sheet_name='DOE')
 
+    n_rows = int((x_range[1] - x_range[0]) / freq_step - 1)
+    processed = np.empty((len(filenames), n_rows, input_size + output_size))
     for file_idx in tqdm(range(len(filenames))):
         filename = filenames[file_idx]
+        # Get XX FRF based on filename of XX FRF
         xx_file = filename.split('_')
         xx_file[2] = 'XX'
         xx_file[0] = str(int(xx_file[0]) + 48)
         xx_number = int(xx_file[0])
         xx_file = '_'.join(xx_file)
+        # Load data
         yy_frf = np.loadtxt('{}/{}'.format(data_dir, filename), delimiter=delimiter)
         xx_frf = np.loadtxt('{}/{}'.format(data_dir, xx_file), delimiter=delimiter)
+        # Truncate data to relevant frequency range
         xx_frf = xx_frf[(np.where((xx_frf[:, 0] > x_range[0]) & (xx_frf[:, 0] < x_range[1])))]
         yy_frf = yy_frf[(np.where((yy_frf[:, 0] > x_range[0]) & (yy_frf[:, 0] < x_range[1])))]
 
+        # Get pose features
         pos_label = doe.loc[doe['XX_Nr'] == xx_number]['Position'].values[0]
         b_angle = doe.loc[doe['XX_Nr'] == xx_number]['B_angle'].values[0]
         x_pos, y_pos = positions.loc[positions['Label'] == pos_label][['X', 'Y']].values[0]
         title = 'X{}_Y{}_B{}'.format(x_pos, y_pos, b_angle)
 
-        data = [xx_frf, yy_frf]
-
+        # Plot FRF
         if len(sys.argv) > 1 and sys.argv[1] == 'plot':
-            plot_frf(data, plot_dir, x_range, y_range, title, ['XX', 'YY'], figsize, fontsize)
+            plot_frf(
+                [xx_frf, yy_frf],
+                plot_dir,
+                x_range,
+                y_range,
+                title,
+                ['XX', 'YY'],
+                figsize,
+                fontsize
+            )
+
+        # Use pose and frequency as features and XX/YY amplitudes as targets,
+        # but store them for each file separately for reasonable train/test split
+        for freq_idx, __ in enumerate(xx_frf):
+            processed[file_idx, freq_idx] = np.array(
+                [
+                    x_pos,
+                    y_pos,
+                    b_angle,
+                    xx_frf[freq_idx, 0],
+                    xx_frf[freq_idx, 1],
+                    yy_frf[freq_idx, 1]
+                ]
+            )
+
+    np.save('{}/processed_data.npy'.format(processed_dir), processed)
 
 if __name__ == '__main__':
     misc.to_local_dir('__file__')
