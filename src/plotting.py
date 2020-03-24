5b254cf1f1bfe055d55407017eb0dcebeacc086a
Felix Finkeldey <felix.finkeldey@tu-dortmund.de>
2020-03-24 14:33:54 +0100

    improved frf plotting interface

--- src/plotting.py
+++ src/plotting.py
@@ -5,88 +5,83 @@ import matplotlib
 from matplotlib import rc
 from matplotlib import pyplot as plt
 from plotter import modify_axis
-
+from properties import (
+   plot_dir,
+   x_range,
+   y_range_amp,
+   y_range_phase,
+   figsize,
+   fontsize
+)
 
 rc('font', family='Arial')
 matplotlib.rcParams.update({'font.size': 14})
 
-DARK2 = [(217 / 255, 95 / 255, 2 / 255),
-         (27 / 255, 158 / 255, 119 / 255),
-         (117 / 255, 112 / 255, 179 / 255),
-         (231 / 255, 41 / 255, 138 / 255),
-         (102 / 255, 166 / 255, 30 / 255),
-         (230 / 255, 171 / 255, 2 / 255),
-         (166 / 255, 118 / 255, 29 / 255),
-         (102 / 255, 102 / 255, 102 / 255)]
 
-def plot_frf(data, plot_dir, x_range, y_range, title, axs_labels, figsize, fontsize):
+def plot_frf(data, labels, colors, linestyles, title):
     """
     Function for plotting of FRFs.
 
     Args:
-      x_range: Range of the x-axis of the plot
-      y_range: Range of the y-axis of the plot
-      figsize: Size of the figure
-      fontsize: Size of the fonts
+        data: List of numpy arrays. Each list member is expected to be a data set to plot.
+            Each data set is expected to contain members for frequency, amplitude and phase.
+        labels: Labels of the data sets.
+        title: Title of the plot. Also used as filename.
     """
-    fig, axs = plt.subplots(len(data), 1, sharex=True, figsize=figsize)
-    for idx, axis_data in enumerate(data):
-        axs[idx].plot(axis_data[:, 0], axis_data[:, 1], color=DARK2[0])
-        axs[idx].plot(axis_data[:, 0], axis_data[:, 1], color=DARK2[1])
-    for idx, axis in enumerate(axs):
-        axis.set_yticks(np.arange(y_range[0], y_range[1], y_range[2]))
-        axis.set_ylim(y_range[0], y_range[1])
+    fig, axs = plt.subplots(2, 1, sharex=True, figsize=figsize)
+    for idx, data_set in enumerate(data):
+        axs[0].plot(
+            data_set[:, 0],
+            data_set[:, 1],
+            linestyle=linestyles[idx],
+            color=colors[idx],
+            label=labels[idx]
+        )
+        axs[1].plot(
+            data_set[:, 0],
+            data_set[:, 2],
+            linestyle=linestyles[idx],
+            color=colors[idx],
+            label=labels[idx]
+        )
+
+    for __, axis in enumerate(axs):
         axis.set_xlim(x_range[0], x_range[1])
         axis.set_xticks(np.arange(x_range[0], x_range[1], x_range[2]))
-        axis.set_title(axs_labels[idx], fontsize=fontsize + 2)
+    axs[0].set_yticks(np.arange(y_range_amp[0], y_range_amp[1], y_range_amp[2]))
+    axs[0].set_ylim(y_range_amp[0], y_range_amp[1])
+    axs[1].set_yticks(np.arange(y_range_phase[0], y_range_phase[1], y_range_phase[2]))
+    axs[1].set_ylim(y_range_phase[0], y_range_phase[1])
     axs[-1].set_xlabel("Frequency")
 
     fig.canvas.draw()
-    for idx, axis in enumerate(axs):
-        axis.set_ylabel("Compliance")
-        axs[idx] = modify_axis(axis, 'Hz', r'$\frac{µm}{N}$', -2, -2, fontsize)
+    axs[0].set_ylabel("Compliance")
+    axs[0] = modify_axis(axs[0], 'Hz', r'$\frac{µm}{N}$', -2, -2, fontsize)
+    axs[1].set_ylabel("Phase angle")
+    axs[1] = modify_axis(axs[1], 'Hz', '°', -2, -2, fontsize)
 
-    fig.suptitle(title, fontsize=fontsize + 4)
+    fig.suptitle(title, fontsize=fontsize + 4, y=1.08)
+    
+    for axis in axs:
+        axis.get_yaxis().set_label_coords(-0.1,0.5)
+
+    fig.tight_layout()
+    leg = axs[0].legend(
+        prop={'size':fontsize},
+        bbox_to_anchor=(0.95, 1.1),
+        bbox_transform=plt.gcf().transFigure
+    )
+    leg.get_frame().set_linewidth(0.0)
+
+    plt.setp(axs[0].get_xticklabels(), visible=False)
 
     plt.savefig(
         '{}/frf_{}.png'.format(plot_dir, title),
         format='png',
-        dpi=600
+        dpi=600,
+        bbox_extra_artists=(leg,),
+        bbox_inches='tight'
     )
     plt.close()
     # plt.show()
-def plot_frf_phase(data, plot_dir, x_range, y_range, title, axs_labels, figsize, fontsize):
-    """
-    Function for plotting of FRFs.
 
-    Args:
-      x_range: Range of the x-axis of the plot
-      y_range: Range of the y-axis of the plot
-      figsize: Size of the figure
-      fontsize: Size of the fonts
-    """
-    fig, axs = plt.subplots(len(data), 1, sharex=True, figsize=figsize)
-    for idx, axis_data in enumerate(data):
-        axs[idx].plot(axis_data[:, 0], axis_data[:, 2], color=DARK2[0])
-        axs[idx].plot(axis_data[:, 0], axis_data[:, 2], color=DARK2[1])
-    for idx, axis in enumerate(axs):
-        axis.set_yticks(np.arange(y_range[0], y_range[1], y_range[2]))
-        axis.set_ylim(y_range[0], y_range[1])
-        axis.set_xlim(x_range[0], x_range[1])
-        axis.set_xticks(np.arange(x_range[0], x_range[1], x_range[2]))
-        axis.set_title(axs_labels[idx], fontsize=fontsize + 2)
-    axs[-1].set_xlabel("Frequency")
-
-    fig.canvas.draw()
-    for idx, axis in enumerate(axs):
-    #    axis.set_ylabel("Compliance")
-        axs[idx] = modify_axis(axis, 'Hz', r'$\frac{µm}{N}$', -2, -2, fontsize)
-
-    fig.suptitle(title, fontsize=fontsize + 4)
-
-    plt.savefig(
-        '{}/frf_phase_{}.png'.format(plot_dir, title),
-        format='png',
-        dpi=600
-    )
-    plt.close()
